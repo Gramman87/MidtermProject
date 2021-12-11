5c9a119aa6eb62fe4ef05122c32dd89597bb737a
Jack H <jack.himes3246@gmail.com>
2021-12-10 18:41:21 -0700

    Create and test User entity, configure hibernate

--- Ho-Ho-HomiesJPA/src/main/java/com/skilldistillery/hohohomies/entities/User.java
+++ Ho-Ho-HomiesJPA/src/main/java/com/skilldistillery/hohohomies/entities/User.java
@@ -1,5 +1,61 @@
 package com.skilldistillery.hohohomies.entities;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String email;
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + "]";
+	}
 
 }
