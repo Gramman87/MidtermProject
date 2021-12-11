593ec6737cc14ff8f2bafde9cd6fee2846e45a87
Gramman87 <gramman87@gmail.com>
2021-12-10 17:03:46 -0700

    Create Spring Boor MVC project

---
+++ Ho-Ho-Homies/src/main/java/com/skilldistillery/hohohomies/HoHoHomiesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hohohomies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoHoHomiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoHoHomiesApplication.class, args);
+	}
+
+}
---
+++ Ho-Ho-Homies/src/test/java/com/skilldistillery/hohohomies/HoHoHomiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hohohomies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoHoHomiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
