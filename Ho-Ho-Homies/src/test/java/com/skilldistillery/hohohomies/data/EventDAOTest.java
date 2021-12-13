300c646012982c0d947b2818fa45174b4e6a2563
Jack H <jack.himes3246@gmail.com>
2021-12-13 14:20:17 -0700

    Created EventDAO junit test

--- Ho-Ho-Homies/src/test/java/com/skilldistillery/hohohomies/data/EventDAOTest.java
+++ Ho-Ho-Homies/src/test/java/com/skilldistillery/hohohomies/data/EventDAOTest.java
@@ -1,14 +1,35 @@
 package com.skilldistillery.hohohomies.data;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.skilldistillery.hohohomies.entities.Event;
+
+@SpringBootTest
 class EventDAOTest {
+	
+	@Autowired
+	EventDAO eventDao;
+	Event event;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	    event = eventDao.findEventFromEventId(1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	    event = null;
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_findEventByEventId() {
+		assertNotNull(event);
 	}
 
 }
