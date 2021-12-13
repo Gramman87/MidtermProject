9cd0e2fcc19aafc1dddbd35dbaa1307c4821b497
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-12-13 12:34:06 -0700

    updated register method on home controller

--- Ho-Ho-Homies/src/main/java/com/skilldistillery/hohohomies/controllers/HomeController.java
+++ Ho-Ho-Homies/src/main/java/com/skilldistillery/hohohomies/controllers/HomeController.java
@@ -3,9 +3,12 @@ package com.skilldistillery.hohohomies.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.hohohomies.data.UserDAO;
+import com.skilldistillery.hohohomies.entities.User;
 
 @Controller
 public class HomeController {
@@ -22,6 +25,19 @@ public class HomeController {
 	@RequestMapping(path="register.do")
 	public String registerAccount() {
 		return "register";
+	}
+
+	@RequestMapping(path="register.do")
+	public ModelAndView registerAccount(@ModelAttribute("user") User user) {
+		ModelAndView mv = new ModelAndView();
+		User newUser = userDao.createUser(user);
+		if(newUser != null) {
+			mv.addObject("newUser", newUser);
+			mv.setViewName("userDashboard");
+		}
+		
+		
+		return mv;
 	}
 
 }
