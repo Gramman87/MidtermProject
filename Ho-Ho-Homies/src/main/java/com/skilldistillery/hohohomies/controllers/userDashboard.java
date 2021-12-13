da1284adafd3b70e5ccfe5cf920f844c0fdffbcf
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-12-13 12:16:35 -0700

    created register jsp, user dashboard jsp, and mapped them to home controller and userdashboard controller

--- Ho-Ho-Homies/src/main/java/com/skilldistillery/hohohomies/controllers/userDashboard.java
+++ Ho-Ho-Homies/src/main/java/com/skilldistillery/hohohomies/controllers/userDashboard.java
@@ -2,26 +2,19 @@ package com.skilldistillery.hohohomies.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.hohohomies.data.UserDAO;
 
 @Controller
-public class HomeController {
-
+public class userDashboard {
+	
 	@Autowired
 	private UserDAO userDao;
 	
-	@RequestMapping(path= {"/", "home.do"})
-	public String home(Model model) {
-		//model.addAttribute("DEBUG", userDao.findByUsername("admin@gmail.com"));
-		return "home";
-	}
-	
-	@RequestMapping(path="register.do")
-	public String registerAccount() {
-		return "register";
+	@RequestMapping(path="userDashboard.do")
+	public String userDashboard() {
+		return "userDashboard";
 	}
 
 }
--- Ho-Ho-Homies/src/main/webapp/WEB-INF/home.jsp
+++ Ho-Ho-Homies/src/main/webapp/WEB-INF/home.jsp
@@ -10,7 +10,23 @@
 
 <h1>Ho-Ho-Homies Gift Exchange</h1>
 
-<h2>User: ${DEBUG}</h2>
+<h2>Welcome to your best event organizer</h2>
+
+	<form action="login.do" method="GET">
+		User Name: <input type="text" name="email" size="20"/> 
+		<br>
+		Password: <input type="text" name="password" size="20"/>
+		<br>
+		<input type="submit" value="Log In" />
+	</form>
+	<br>
+	<br>
+	<p>Need an account? 
+		<form action="register.do" method="GET">
+		<input type="submit" value="Register" />
+	</form>
+
+
 
 </body>
 </html>
