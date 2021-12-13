c6bbc917a2a7de4ac01ddca932f2bd92a9312b47
Jack H <jack.himes3246@gmail.com>
2021-12-13 14:05:07 -0700

    Updated user roles, changed Address_id to nullable

---
+++ Ho-Ho-Homies/src/test/java/com/skilldistillery/hohohomies/data/EventDAOTest.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.hohohomies.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EventDAOTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
