c72df75c9707434588c72a35261c3ef0e9ef46d1
Gramman87 <gramman87@gmail.com>
2021-12-14 13:59:10 -0700

    Wishlist controller and .jsp

--- Ho-Ho-Homies/src/main/java/com/skilldistillery/hohohomies/controllers/WishlistController.java
+++ Ho-Ho-Homies/src/main/java/com/skilldistillery/hohohomies/controllers/WishlistController.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.hohohomies.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.skilldistillery.hohohomies.data.WishlistItemDAO;
+import com.skilldistillery.hohohomies.entities.User;
+
+@Controller
 public class WishlistController {
+	
+	@Autowired
+	private WishlistItemDAO listDao;
+	
+	@RequestMapping(path = "wishlist.do", method = RequestMethod.POST)
+	public String wishlistDisplay(User user, Model model) {
+		
+		model.addAllAttributes(listDao.findWishlistByUser(user));
+		
+		return "wishlist";
+	}
 
 }
